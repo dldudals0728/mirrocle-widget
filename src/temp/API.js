fc4d4ecea15b748e481c38e758faa8b6dd0f07e5
Young Min <dldudals0728@naver.com>
2022-11-30 12:34:09 +0900

    Done. mirrocle-widgets project

---
+++ src/temp/API.js
@@ -0,0 +1,5 @@
+export const API_KEYS = {
+  openweathermap: "b7091e2d*****************",
+  seoulSubwayRealTime: "	6e736*************************",
+  seoulSubwayStationInfo: "467649***************************",
+};
